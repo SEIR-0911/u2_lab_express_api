14f280b00254151a899c25a71f97b5abb322734e
Rhys Frazier <rhysrfrazier@gmail.com>
2023-10-08 19:43:03 -0400

    bonus 1

--- controllers/actorController.js
+++ controllers/actorController.js
@@ -2,7 +2,10 @@ const { Actor } = require('../models')
 
 module.exports = {
     getAllActors,
-    getOneActor
+    getOneActor,
+    createActor,
+    updateActor,
+    deleteActor
 }
 
 async function getAllActors(req,res){
@@ -24,5 +27,43 @@ async function getOneActor(req,res){
         return res.status(400).send('This database is to small for that')
     } catch (e) {
         return res.status(500).send('Server error')
+    }
+}
+
+async function createActor(req, res) {
+    try {
+        const actor = new Actor(req.body)
+        await actor.save()
+        return res.status(201).json({
+            actor
+        })
+    }catch (e) {
+        return res.status(500).send(e.message)
+    }
+}
+
+async function updateActor(req,res){
+    try{
+        const input = req.params.name
+        const actor = await Actor.findOneAndUpdate({name: input}, req.body, {new: true})
+        if (actor) {
+            return res.status(200).json(actor)
+        }
+        throw new Error('Actor not found')
+    } catch(e){
+        return res.status(500).send(e.message)
+    }
+}
+
+async function deleteActor(req,res){
+    try{
+        const input = req.params.name
+        const actor = await Actor.findOneAndDelete({name: input})
+        if (actor){
+            return res.status(200).json(actor)
+        }
+        throw new Error ('Actor not found')
+    } catch (e) {
+        return res.status(500).send(e.message)
     }
 }
--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,12 +2,16 @@ const { Review } = require('../models')
 
 module.exports = {
     getAllReviews,
-    getOneReview
+    getOneReview,
+    createReview,
+    updateReview,
+    deleteReview
 }
 
 async function getAllReviews(req,res){
     try{
         const reviews = await Review.find()
+        reviews.sort(function(reviewA,reviewB){return reviewB.stars - reviewA.stars}) //https://www.w3schools.com/js/js_array_sort.asp descending order
         return res.json(reviews)
     } catch(e){
         return res.status(500).send('Server error')
@@ -24,5 +28,43 @@ async function getOneReview(req,res){
         return res.status(400).send('This database is too small for that')
     } catch (e) {
         return res.status(500).send('Server error')
+    }
+}
+
+async function createReview(req, res) {
+    try {
+        const review = new Review(req.body)
+        await review.save()
+        return res.status(201).json({
+            review
+        })
+    }catch (e) {
+        return res.status(500).send(e.message)
+    }
+}
+
+async function updateReview(req,res){
+    try{
+        const id = req.params.id
+        const review = await Review.findByIdAndUpdate(id, req.body, {new: true})
+        if (review) {
+            return res.status(200).json(review)
+        }
+        throw new Error('Review not found')
+    } catch(e){
+        return res.status(500).send(e.message)
+    }
+}
+
+async function deleteReview(req,res){
+    try{
+        const id = req.params.id
+        const review = await Review.findByIdAndDelete(id)
+        if (review){
+            return res.status(200).json(review)
+        }
+        throw new Error ('Review not found')
+    } catch (e) {
+        return res.status(500).send(e.message)
     }
 }
