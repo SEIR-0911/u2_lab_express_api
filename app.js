f76e71d3959941ebed0ccc7367af409fd29db5e2
KLoCascio <KyndalLife@gmail.com>
2023-10-08 20:33:59 -0400

    structure setup and host works

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express')
-const db = require('./db')
+const db = require('./db/index')
 const logger = require('morgan')
 const bodyParser = require('body-parser')
+
+const PORT = process.env.PORT || 3001
+
+const app = express()
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.send('Root Setup')
 })
+
+app.listen(PORT, () => console.log(`Listening on Port: ${PORT}`))
--- db/index.js
+++ db/index.js
@@ -1,8 +1,7 @@
 // connect moviesDatabase using Mongoose boilerplate aka:
 // create a mongoose const, then mongoose.connect.then.catch, then db and exports
 
-const e = require("express")
-const mongoose = require("mongoose")
+const mongoose = require('mongoose')
 
 mongoose
 .connect('mongodb://127.0.0.1:27017/moviesDatabase')
